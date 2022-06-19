42bc86483a067e3303e6caa8492c685a58fcbb82
giolandicho <giolandicho@gmail.com>
2022-06-19 00:21:14 -0700

    added signup

--- src/MockAPI.js
+++ src/MockAPI.js
@@ -5,19 +5,19 @@ const url = "https://62a25ccccd2e8da9b0071d4e.mockapi.io";
 
 export const getUser = async() => {
     try{
-        const response = await axios.get(`${url}/user/1`)
+        const response = await axios.get(`${url}/users/1`)
         return response.data;
     } catch(err) {
         console.log(err)
     }
 }
-export const addUser = (email, password) => {
-    axios.post(`${url}/user`, {
+export const addUser = async(email, password) => {
+   try{ axios.post(`${url}/user`, {
         email: email,
         password: password
     }).then((res) => {
         console.log(res.data);
-    }).catch((err) => {
-        console.log(err)
-    })
+    })} catch(err){
+            console.log(err);
+    }
 }
